86aa29457836e4764c7aec724bb274c12c6fc67a
Josh Pewtress <Joshpewtress@gmail.com>
2023-04-03 19:42:08 -0600

    Changed internal class's to public and remade the StoreItems class

--- CKK.Logic/Models/StoreItem.cs
+++ CKK.Logic/Models/StoreItem.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace CKK.Logic.Models
 {
-    internal class StoreItem
+    public class StoreItem
     {
+      private Product Product;
+      private int Quantity;
+
+      public StoreItem(Product product, int quantity) 
+      {
+         Product = product;
+         Quantity = quantity;
+      }
+
+      public int GetQuantity()
+      {
+         return Quantity;
+      }
+
+      public void SetQuantity(int quantity)
+      {
+         Quantity = quantity;
+      }
+
+      public Product GetProduct()
+      {
+         return Product;
+      }
+
+      public void SetProduct(Product product)
+      {
+         Product = product;
+      }
     }
 }
