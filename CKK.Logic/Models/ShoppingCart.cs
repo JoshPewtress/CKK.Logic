17b5f30f4fe4c8fb7a027f4ca5c4eeed85dfa5ee
Josh Pewtress <Joshpewtress@gmail.com>
2023-05-09 19:58:37 -0600

    Rewrote the Store and ShoppingCart classes to use a List<t> instead of products to let the shoppingcart expand past 3 products

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -8,190 +8,93 @@ namespace CKK.Logic.Models
 {
    public class ShoppingCart
    {
-      private Customer _Customer;
-      private ShoppingCartItem _product1;
-      private ShoppingCartItem _product2;
-      private ShoppingCartItem _product3;
+      private Customer _customer;
+      private List<ShoppingCartItem> _products = new List<ShoppingCartItem>();
 
       public ShoppingCart(Customer cust) 
       {
-         _Customer = cust;
+         _customer = cust;
       }
 
-      // Returns customer ID that was given from constructor
       public int GetCustomerId()
       {
-         return _Customer.GetId();
+         return _customer.GetId();
       }
-      
-      // GetProductById returns the product that matches the id that was entered
-      public ShoppingCartItem GetProductById(int id)
+
+      public ShoppingCartItem AddProduct(Product prod, int quantity)
       {
-         if (id == _product1.GetProduct().GetId())
+         // Check list for product matching Id
+         foreach (var item in _products)
          {
-            return _product1;
-         }
-         else if (id == _product2.GetProduct().GetId())
-         {
-            return _product2;
-         }
-         else if (id == _product3.GetProduct().GetId())
-         {
-            return _product3;
+            // Validate quantity is not negative
+            if (quantity >= 0)
+            {
+               // Found product, add to quantity
+               if (item.GetProduct().GetId() == prod.GetId())
+               {
+                  item.SetQuantity(item.GetQuantity() + quantity);
+                  return item;
+               }
+            }
          }
 
+         ShoppingCartItem newCartItem = new ShoppingCartItem(prod, quantity);
+         _products.Add(newCartItem);
+         return newCartItem;
+      }
+
+      public ShoppingCartItem RemoveProduct(int id, int quantity)
+      {
+         // Iterate through _products list
+         foreach (var item in _products)
+         {
+            // Validate provided quantity
+            if (quantity >= 0)
+            {
+               // Checks if list contains the Id
+               if (item.GetProduct().GetId() == id)
+               {
+                  // Item quantity is enough to remove
+                  if (item.GetQuantity() >= quantity)
+                  {
+                     item.SetQuantity(item.GetQuantity() - quantity);
+                     return item;
+                  }
+                  // Quantity to remove is too high, remove item
+                  else
+                  {
+                     _products.Remove(item);
+                  }
+               }
+            }
+         }
+         // Product was not found
          return null;
       }
 
-      // AddProduct method overload
-      public ShoppingCartItem AddProduct(Product prod)
+      public ShoppingCartItem GetProductById(int id)
       {
-         return AddProduct(prod, 1);
-      }
-
-      /* 
-       * AddProduct method adds quantity to the product if quantity is valid
-       * and returns the changed product or null
-      */ 
-      public ShoppingCartItem AddProduct(Product prod, int quantity)
-      {
-         // Checking for valid quantity
-         if (quantity < 0)
+         foreach (var item in _products)
          {
-            return null;
-         }
-
-         // Checking if product exists and adding quantity to it if it does
-         if (_product1 != null && prod.GetId() == _product1.GetProduct().GetId())
-         {
-            // Getting quantity from product and adding quantity to it
-            _product1.SetQuantity(_product1.GetQuantity() + quantity);
-
-            return _product1;
-         }
-         else if (_product2 != null && prod.GetId() == _product2.GetProduct().GetId())
-         {
-            _product2.SetQuantity(_product2.GetQuantity() + quantity);
-
-            return _product2;
-         }
-         else if (_product3 != null && prod.GetId() == _product3.GetProduct().GetId())
-         {
-            _product3.SetQuantity(_product3.GetQuantity() + quantity);
-
-            return _product3;
-         }
-         // No matches, checking for empty product and create a new product if one is found
-         else
-         {
-            // Checking for empty product
-            if (_product1 == null)
+            // Id found in the list
+            if (item.GetProduct().GetId() == id)
             {
-               // Setting product to prod and quantity to quantity
-               _product1 = new ShoppingCartItem(prod, quantity);
-
-               return _product1;
+               return item;
             }
-            else if (_product2 == null)
-            {
-               _product2 = new ShoppingCartItem(prod, quantity);
-
-               return _product2;
-            }
-            else if (_product3 == null)
-            {
-               _product3 = new ShoppingCartItem(prod, quantity);
-
-               return _product3;
-            }
-
-            // Unable to change anything
-            return null;
          }
-
-         
+         // Id not found in the list
+         return null;
       }
-
-      /*
-       * RemoveProduct method checks for valid quantity and removes it from a product
-       */ 
-      public ShoppingCartItem RemoveProduct(Product prod, int quantity)
-      {
-         // Verifying positive quantity
-         if (quantity <= 0)
-         {
-            return null;
-         }
-
-         // Checking if product exists and removes quantity from it if it does
-         if (prod.GetId() == _product1.GetProduct().GetId())
-         {
-            // Getting quantity from product and removing quantity from it
-            _product1.SetQuantity(_product1.GetQuantity() - quantity);
-
-            return _product1;
-         }
-         else if (prod.GetId() == _product2.GetProduct().GetId())
-         {
-            _product2.SetQuantity(_product2.GetQuantity() - quantity);
-
-            return _product2;
-         }
-         else if (prod.GetId() == _product3.GetProduct().GetId())
-         {
-            _product3.SetQuantity(_product3.GetQuantity() - quantity);
-
-            return _product3;
-         }
-         else
-         {
-            return null;
-         }
-      }
-
-      // GetTotal returns the total cost of all products
+      
       public decimal GetTotal()
       {
-         // Variable to store value
-         decimal total = 0;
-
-         // If product is empty, total will not store its total
-         if (_product1 != null)
-         {
-            total += _product1.GetTotal();
-         }
-         if (_product2 != null)
-         {
-            total += _product2.GetTotal();
-         }
-         if (_product3 != null)
-         {
-            total += _product3.GetTotal();
-         }
-
+         decimal total = _products.Sum(item => item.GetTotal());
          return total;
       }
 
-      // GetProduct returns the product that matches prodNum
-      public ShoppingCartItem GetProduct(int prodNum)
+      public List<ShoppingCartItem> GetProducts()
       {
-         // Verifying input is correct
-         if (prodNum < 1 && prodNum > 3)
-         {
-            return null;
-         }
-
-         switch (prodNum)
-         {
-            case 1:
-               return _product1;
-            case 2:
-               return _product2;
-            case 3:
-               return _product3;
-            default:
-               return null;
-         }
+         return _products;
       }
    }
 }
