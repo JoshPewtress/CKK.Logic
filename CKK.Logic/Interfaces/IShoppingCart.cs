c768566bd648bd2239ad4003d8ae83695a362cc7
Josh Pewtress <Joshpewtress@gmail.com>
2023-05-29 08:03:31 -0600

    Created Interfaces IStore and IShoppingCart, Utilized the interfaces in Store and ShoppingCart classes respectfully

---
+++ CKK.Logic/Interfaces/IShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using CKK.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Interfaces
+{
+   interface IShoppingCart
+   {
+      int GetCustomerId();
+
+      ShoppingCartItem AddProduct(Product prod, int quantity);
+
+      ShoppingCartItem RemoveProduct(int id, int quantity);
+
+      decimal GetTotal();
+
+      ShoppingCartItem GetProductById(int id);
+
+      List<ShoppingCartItem> GetProducts();
+   }
+}
---
+++ CKK.Logic/Interfaces/IStore.cs
@@ -0,0 +1,20 @@
+﻿using CKK.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Interfaces
+{
+   interface IStore
+   {
+      StoreItem AddStoreItem(Product prod, int quantity);
+
+      StoreItem RemoveStoreItem(int id, int quantity);
+
+      StoreItem FindStoreItemById(int id);
+
+      List<StoreItem> GetStoreItems();
+   }
+}
